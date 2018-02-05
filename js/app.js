14b9cce6b1a52fdb6a5e90301609d350529ae7cf
Romaric Anquetil <romaric.anquetil@gmail.com>
2018-02-05 18:35:06 +0100

    add goUp and goDown functions

--- js/app.js
+++ js/app.js
@@ -5,4 +5,24 @@ class Elevator {
             (this.minFloor = minFloor),
             (this.maxFloor = maxFloor);
     }
+
+    // fonction qui permet à mon ascenseur de me monter
+    goUp() {
+        // je verifie si l'ascenseur n'est pas deja au dernier étage
+        if (this.currentFloor < this.maxFloor) {
+            this.currentFloor += 1;
+        } else {
+            alert("Vous ne pouvez pas monter plus haut");
+        }
+    }
+
+    // fonction qui permet à mon ascenseur de me descendre
+    goDown() {
+        // je verifie si l'ascenseur n'est pas deja au plus bas etage
+        if (this.currentFloor > this.minFloor) {
+            this.currentFloor -= 1;
+        } else {
+            alert("Vous ne pouvez pas descendre plus bas");
+        }
+    }
 }

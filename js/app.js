511b780aef59fe0cc520d94e57eb8cb11252977d
Romaric Anquetil <romaric.anquetil@gmail.com>
2018-02-05 18:34:21 +0100

    add elevator class

---
+++ js/app.js
@@ -0,0 +1,8 @@
+class Elevator {
+    // constructeur de la class Elevator
+    constructor(minFloor, maxFloor) {
+        (this.currentFloor = 0),
+            (this.minFloor = minFloor),
+            (this.maxFloor = maxFloor);
+    }
+}
